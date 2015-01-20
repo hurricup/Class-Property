5e727a3c263fcbfaf49e384ebb3291501c96c923
evstigneev <evstigneev@220-volt.ru>
2015-01-20 17:47:12 +0300

    Cleanup

--- t/Foo.pm
+++ t/Foo.pm
@@ -3,11 +3,11 @@ use Class::Property;
 use parent 'Class::Property';
 
 property(
-    'price' => { 'get' => undef, 'set' => 'default' }                           # default setter and getter
-    , 'weight' => { 'get' => 'anyscalar' }                                      # default gettter, RO property
+    'price' => { 'get' => undef, 'set' => undef }                               # default setter and getter
+    , 'weight' => { 'get' => undef }                                            # default gettter, RO property
     , 'quantity' => { 'set' => undef }                                          # default settter, WO property
     , 'parameters' => { 'get' => \&get_parameters, 'set' => \&set_parameters }  # custom getter and setter
-    , 'param2' => {'lazy' => \&get_param2, 'set' => undef }                     # default lazy getter with get_param2 as lazy init and default setter
+    , 'param2' => { 'lazy' => \&get_param2, 'set' => undef }                    # default lazy getter with get_param2 as lazy init and default setter
 );
 
 sub get_parameters
